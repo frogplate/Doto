7ef2fe8ff139c4dc8920adcfa5050602abe83dc4
Jonathan Littlewood <frogplate@gmail.com>
2020-12-27 23:07:10 +0000

    Add help for 'add' command

--- doto.ts
+++ doto.ts
@@ -1,10 +1,19 @@
-import { summary, help } from "./help.ts";
+// import { summary, help, helpAdd } from "./help.ts";
+import * as help from "./help.ts";
 
 if (Deno.args.length == 0) {
-  await summary();
+  await help.summary();
 } else {
-  if (Deno.args.includes('-h')) {
-    await help();
+  switch (Deno.args[0].toLowerCase()) {
+    case "-h":
+    case "--help":
+      await help.full();
+      break;
+    case "help":
+      await help.cmd();
+      break;
+    default:
+      await help.summary();
   }
 }
 
--- help.ts
+++ help.ts
@@ -3,12 +3,35 @@ export async function summary() {
   console.log("Try 'doto -h' for more information.");
 }
 
-export async function help() {
+export async function full() {
   console.log("Usage: doto [-h] action [task_number] [task_description]");
   console.log();
   console.log("Actions:");
-  console.log('  add|a "THING I NEED TO DO +project @context"');
+  console.log('  add | a "action that needs carrying out +project @context"');
   console.log();
   console.log('See "help" for more details.');
 }
 
+export async function add() {
+  console.log('add "action that needs carrying out +project @context"');
+  console.log('a "action that needs carrying out +project @context"');
+  console.log('  Adds "action that needs carrying out" to your todo.txt file on a new line.');
+  console.log('  Project and context tags are optional.');
+  console.log('  Quotes are optional.');
+}
+
+export async function cmd() {
+  if (Deno.args.length > 1) {
+    const cmd = Deno.args[1].toLowerCase();
+    switch (cmd) {
+      case "a":
+      case "add":
+        await add();
+        break;
+      default:
+        console.log('DOTO: No action "' + cmd + '" exists.');
+    }
+  } else {
+    await summary();
+  }
+}
