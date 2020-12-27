a6ab5bcdb88f4b67ca65eb6883f8411d7b9a90a9
Jonathan Littlewood <frogplate@gmail.com>
2020-12-27 14:39:24 +0000

    Initial commit

---
+++ doto.ts
@@ -0,0 +1,7 @@
+async function summary () {
+  console.log("Usage: doto [-h] action [task_number] [task_description]");
+  console.log("Try 'doto -h' for more information.");
+}
+
+await summary();
+
