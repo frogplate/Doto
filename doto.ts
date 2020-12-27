6869a22a317f3058e4495156500bdbf1a19a540e
Jonathan Littlewood <frogplate@gmail.com>
2020-12-27 14:59:23 +0000

    Add main help page

--- doto.ts
+++ doto.ts
@@ -1,7 +1,22 @@
-async function summary () {
+async function summary() {
   console.log("Usage: doto [-h] action [task_number] [task_description]");
   console.log("Try 'doto -h' for more information.");
 }
 
-await summary();
+async function help() {
+  console.log("Usage: doto [-h] action [task_number] [task_description]");
+  console.log();
+  console.log("Actions:");
+  console.log('  add|a "THING I NEED TO DO +project @context"');
+  console.log();
+  console.log('See "help" for more details.');
+}
+
+if (Deno.args.length == 0) {
+  await summary();
+} else {
+  if (Deno.args.includes('-h')) {
+    await help();
+  }
+}
 
